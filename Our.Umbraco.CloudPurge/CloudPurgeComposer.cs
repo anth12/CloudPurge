e6a76c1f4d09e70aaf80cf26c282e4e103e23f64
Anthony Halliday <anthony.halliday@outlook.com>
2021-05-06 21:57:26 +0100

    Updated composer DI

--- Our.Umbraco.CloudPurge/CloudPurgeComposer.cs
+++ Our.Umbraco.CloudPurge/CloudPurgeComposer.cs
@@ -1,13 +1,15 @@
-﻿using Our.Umbraco.CloudPurge.Config;
-using Our.Umbraco.CloudPurge.Controllers;
-using System.Net.Http;
-using System.Runtime.CompilerServices;
-using Our.Umbraco.CloudPurge.Cdn;
-using Our.Umbraco.CloudPurge.CDN.CloudFlare;
-using Our.Umbraco.CloudPurge.Services;
+﻿using System.Runtime.CompilerServices;
+using Our.Umbraco.CloudPurge.Events;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Web.BackOffice.Trees;
+using Umbraco.Cms.Core.Services.Notifications;
+using Microsoft.Extensions.DependencyInjection;
+using Our.Umbraco.CloudPurge.Config;
+using Our.Umbraco.CloudPurge.CDN.CloudFlare;
+using Our.Umbraco.CloudPurge.Services;
+using Our.Umbraco.CloudPurge.Controllers;
+using Our.Umbraco.CloudPurge.Cdn;
 
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("Our.Umbraco.CloudPurge.Tests")]
@@ -15,24 +17,19 @@ namespace Our.Umbraco.CloudPurge
 {
 	public class CloudPurgeComposer : IComposer
 	{
-		public void Compose(Composition composition)
-		{
-		}
-
         public void Compose(IUmbracoBuilder builder)
         {
-			//builder.Register<IConfigService, ConfigFileService>(Lifetime.Singleton);
-			//composition.Register<ICdnApi, CloudFlareV4Api>(Lifetime.Transient);
-			//composition.Register<IContentCdnService, ContentCdnService>(Lifetime.Transient);
-			//composition.Register<CloudPurgeApiController>(Lifetime.Transient);
+			builder.Services.AddSingleton<IConfigService, ConfigFileService>();
+			builder.Services.AddTransient<ICdnApi, CloudFlareV4Api>();
+			builder.Services.AddTransient<IContentCdnService, ContentCdnService>();
+			builder.Services.AddTransient<CloudPurgeApiController>();
 			
 			//composition.Register<HttpClient>(Lifetime.Singleton);
 			
-			builder.Components().Append<CloudPurgeComponent>();
 			builder.Dashboards().Add<CloudPurgeDashboard>();
 
 			builder.AddNotificationHandler<MenuRenderingNotification, MenuRenderingHandler>();
-
+			builder.AddNotificationAsyncHandler<ContentTreeChangeNotification, ContentUpdateHandler>();
 
 		}
 	}
