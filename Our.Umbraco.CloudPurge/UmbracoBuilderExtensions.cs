35c2c23e3b868525a0db70922f0c85ac2b958b50
Anthony Halliday <anthony.halliday@outlook.com>
2021-05-11 21:53:07 +0100

    Umb builder extensions

---
+++ Our.Umbraco.CloudPurge/UmbracoBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Our.Umbraco.CloudPurge.Cdn;
+using Our.Umbraco.CloudPurge.CDN.CloudFlare;
+using Our.Umbraco.CloudPurge.Config;
+using Our.Umbraco.CloudPurge.Controllers;
+using Our.Umbraco.CloudPurge.Services;
+using System;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Our.Umbraco.CloudPurge
+{
+    public static class UmbracoBuilderExtensions
+    {
+        private const string ConfigKey = "CloudPurge";
+
+        public static IUmbracoBuilder AddCloudPurge(this IUmbracoBuilder builder, Action<CloudPurgeConfig> defaultOptions = default)
+        {
+            var options = builder.Services.AddOptions<CloudPurgeConfig>()
+                .Bind(builder.Config.GetSection(ConfigKey));
+
+            if (defaultOptions != default)
+            {
+                options.Configure(defaultOptions);
+            }
+
+            builder.Services.AddTransient<ICdnApi, CloudFlareV4Api>();
+            builder.Services.AddTransient<IContentCdnService, ContentCdnService>();
+            builder.Services.AddTransient<CloudPurgeApiController>();
+
+            //composition.Register<HttpClient>(Lifetime.Singleton);
+            return builder;
+        }
+    }
+}
